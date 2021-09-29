9115bb3829ddca72b18cc41f8236ec04c0d18379
Jenő <bkrisz.100@gmail.com>
2021-09-29 15:24:17 +0200

    Main.java c-ig kész

--- src/Petrik/Main.java
+++ src/Petrik/Main.java
@@ -1,8 +1,15 @@
 package Petrik;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-
+    static List<Bejegyzes> bejegyzesek = new ArrayList<>();
     public static void main(String[] args) {
+        bejegyzesek.add(new Bejegyzes("asd","asd"));
+        bejegyzesek.add(new Bejegyzes("asdasd","asdasdasd"));
+        bejegyzesek.add(new Bejegyzes("asdasdasd","asdasdasdasd"));
 
+        System.out.println(bejegyzesek.toString());
     }
 }
